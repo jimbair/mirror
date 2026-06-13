f20bc7bda1e77769678624abbb81c9232d223423
Jim Bair <james.d.bair@gmail.com>
2026-06-13 10:46:32 -0500

    Update status-update.sh
    Add a few more exit 1 conditions. I'm not convinced we'll hit them, but it might if the regex pukes.

--- status-update.sh
+++ status-update.sh
@@ -14,9 +14,9 @@ echo >> ${TMP} || exit 1 # Formatting
 # Let's sort by ratio; thanks Gemini!
 REMOTE=$(/usr/local/bin/transmission-remote -l)
 [[ -n "${REMOTE}" ]] || exit 1
-head -n 1 <<< ${REMOTE} >> ${TMP}
-sed '1d;$d' <<< ${REMOTE} | sed -E 's/ (GB|MB|kB|B|mins|hrs|days|secs)/@\1/g' | sort -k7rn | sed -E 's/@(GB|MB|kB|B|mins|hrs|days|secs)/ \1/g' >> ${TMP}
-tail -n 1 <<< ${REMOTE} >> ${TMP}
+head -n 1 <<< ${REMOTE} >> ${TMP} || exit 1
+sed '1d;$d' <<< ${REMOTE} | sed -E 's/ (GB|MB|kB|B|mins|hrs|days|secs)/@\1/g' | sort -k7rn | sed -E 's/@(GB|MB|kB|B|mins|hrs|days|secs)/ \1/g' >> ${TMP} || exit 1
+tail -n 1 <<< ${REMOTE} >> ${TMP} || exit 1
 
 # Install the updated status
 cat ${TMP} > /var/lib/transmission/Downloads/status.txt || exit 1 
